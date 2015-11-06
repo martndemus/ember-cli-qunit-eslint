e1c91089703a0a16193f2a3a124716a9d8d1696f
Marten Schilstra <mail@martenschilstra.nl>
2015-11-06 14:14:20 +0100

    Initial implementation

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
 /* jshint node: true */
 'use strict';
 
+var path   = require('path');
+var eslint = require('broccoli-lint-eslint');
+var jsStringEscape = require('js-string-escape');
+
+function renderErrors(errors) {
+  if (!errors || errors.length === 0) {
+    return '';
+  }
+
+  return errors.map(function(error) {
+    return error.line + ':' + error.column + ' - ' +
+      error.message + ' (' + error.ruleId + ')';
+  }).join('\n');
+}
+
+function testGenerator(relativePath, errors) {
+  var pass = !errors || errors.length === 0;
+  return "QUnit.module('ESLint - " + path.dirname(relativePath) + "');\n" +
+         "QUnit.test('" + relativePath + " should pass ESLint', function(assert) { \n" +
+         "  assert.ok(" + pass + ", \"" + relativePath + " shoud pass ESLint.\\n\\n" + jsStringEscape(renderErrors(errors)) + "\");\n" +
+         "});\n";
+}
+
 module.exports = {
-  name: 'ember-cli-qunit-eslint'
+  name: 'ember-cli-qunit-eslint',
+  lintTree: function(type, tree) {
+    return eslint(tree, { testGenerator: testGenerator });
+  }
 };
